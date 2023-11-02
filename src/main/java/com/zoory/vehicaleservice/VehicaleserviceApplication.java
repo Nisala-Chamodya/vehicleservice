e4acf118f5b3cfb2c92a4b741962e931cf8214d2
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2023-11-02 12:47:18 +0530

    application file configurations

---
+++ src/main/java/com/zoory/vehicaleservice/VehicaleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zoory.vehicaleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicaleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicaleserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoory/vehicaleservice/VehicaleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoory.vehicaleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicaleserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
